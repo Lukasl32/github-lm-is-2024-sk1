b88f99675e847db9f700257ab09e831e7f8d28ab
Lukasl32 <Lukasl32@atlas.cz>
2024-10-02 09:48:17 +0200

    Update změn projektu

--- IS-Projekty/001-app1-vypis-rady/obj/Debug/net9.0/001-app1-vypis-rady.AssemblyInfo.cs
+++ IS-Projekty/001-app1-vypis-rady/obj/Debug/net9.0/001-app1-vypis-rady.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("001-app1-vypis-rady")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42411f5b3692d2cd2b769bd257815db3622040de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a7529f79559c0a85ba68bba3ff6687e123ecc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("001-app1-vypis-rady")]
 [assembly: System.Reflection.AssemblyTitleAttribute("001-app1-vypis-rady")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
