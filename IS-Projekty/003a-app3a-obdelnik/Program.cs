436cba2c97a89753e37467f72f5d758026589b87
Lukasl32 <Lukasl32@atlas.cz>
2024-10-16 10:06:28 +0200

    #3 - vytvoření projektů

---
+++ IS-Projekty/003a-app3a-obdelnik/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _003a_app3a_obdelnik;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ IS-Projekty/003b-app3b-pravouhly-trojuhelnik/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _003b_app3b_pravouhly_trojuhelnik;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
