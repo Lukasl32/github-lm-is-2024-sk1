6d2d3227ab88f2a6a8a9f0b57e856a54501d4524
Lukasl32 <Lukasl32@atlas.cz>
2024-10-16 10:21:33 +0200

    #2 - varianta za pomocí string a char

--- IS-Projekty/002b-app2b-soucet-cifer/Program.cs
+++ IS-Projekty/002b-app2b-soucet-cifer/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string repeate = "a";
+            while (repeate.Equals("a"))
+            {
+                Console.Clear();
+                Console.WriteLine("********************************");
+                Console.WriteLine("******** Lukáš Moravec *********");
+
+                int number;
+                Console.Write("Zadejte celé číslo: ");
+                while (!int.TryParse(Console.ReadLine(), out number))
+                    Console.Write("Chybně zadané celé číslo!! \nZadejte znovu: ");
+
+                int sum = 0, product = 1;
+                string numberString = Convert.ToString(number);
+
+                foreach (var item in numberString)
+                {
+                    int digit = Convert.ToInt32(item) - 48; // převedení char na int + korekce odečtem 48, které odpovídá ASCII '0'
+                    sum += digit;     // součet číslic v čísle
+                    product *= digit; // součin číslic v čísle
+                }
+
+                Console.WriteLine($"\nSoučet cifer čísla \"{number}\" je: {Math.Abs(sum)}");
+                Console.WriteLine($"\nSoučin cifer čísla \"{number}\" je: {Math.Abs(product)}");
+
+                Console.ReadKey();
+            }
         }
     }
 }
