9f234ef5cd6f4512714b53e53098b7d20f4cd919
Lukasl32 <Lukasl32@atlas.cz>
2024-10-23 08:53:32 +0200

    #3 - jenoduché vykreslení pravoúhlého trojuhelníku

--- IS-Projekty/003b-app3b-pravouhly-trojuhelnik/Program.cs
+++ IS-Projekty/003b-app3b-pravouhly-trojuhelnik/Program.cs
@@ -4,6 +4,28 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string repeate = "a";
+        while (repeate.Equals("a"))
+        {
+            Console.Clear();
+            Console.WriteLine("********************************");
+            Console.WriteLine("******** Lukáš Moravec *********");
+
+            int height;
+            Console.Write("Zadejte výšku obrazce (celé číslo): ");
+            while (!int.TryParse(Console.ReadLine(), out height))
+                Console.Write("Chybně zadané celé číslo!! \nZadejte znovu: ");
+
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
     }
 }
