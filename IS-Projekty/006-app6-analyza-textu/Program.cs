f6620bd942ad53092f1de6122c93420876e90bff
Lukasl32 <Lukasl32@atlas.cz>
2024-11-06 10:16:11 +0100

    #6 - vypracování úkolu 006 + solution update pro #5

---
+++ IS-Projekty/006-app6-analyza-textu/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _006_app6_analyza_textu;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        char[] vowels = new char[] { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z', 'č', 'ď', 'ř', 'š', 'ť', 'ž' };
+        char[] constatns = ['a', 'e', 'i', 'o', 'u', 'y', 'á', 'é', 'í', 'ó', 'ú', 'ů', 'ý']; // zkrácená deklarace pole
+
+        string repeate = "a";
+        while (repeate.Equals("a"))
+        {
+            int vowlCount = 0, constatCount = 0, otherCharCount = 0;
+
+            Console.Clear();
+            Console.WriteLine("********************************");
+            Console.WriteLine("******** Lukáš Moravec *********");
+
+            Console.Write("Zadejte větu k analýze: ");
+            string? text = Console.ReadLine();
+
+            if (text != null)
+            {
+                text = text.ToLower();
+                foreach (var item in text)
+                {
+                    if (vowels.Contains(item))
+                        vowlCount++;
+                    else if (constatns.Contains(item))
+                        constatCount++;
+                    else
+                        otherCharCount++;
+                }
+            }
+
+            Console.WriteLine("Výsledek analýzy:");
+            Console.WriteLine($"Počet souhlásek: {vowlCount}");
+            Console.WriteLine($"Počet samohlásek: {constatCount}");
+            Console.WriteLine($"Počet ostatních znaků: {otherCharCount}");
+
+            Console.ReadKey();
+        }
+    }
+}
