86d08498cd08da78ab324caaeef7411a0306f9a5
Lukasl32 <Lukasl32@atlas.cz>
2024-12-04 10:08:10 +0100

    #13 - Kód pro převod z DEC na BIN (NEDOKONČENO)

---
+++ IS-Projekty/013-app13-prevod-z-binarni-soustavy/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace _013_app13_prevod_z_binarni_soustavy;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string repeate = "a";
+        bool valid = true;
+        while (repeate.Equals("a"))
+        {
+            Console.Clear();
+            Console.WriteLine("********************************");
+            Console.WriteLine("******** Lukáš Moravec *********");
+
+            uint number, numberBackup;
+            uint[] binNumber = new uint[32];
+
+            Console.Write("Zadejte přirozené číslo v desítkové soustavě [>0]: ");
+            while (!uint.TryParse(Console.ReadLine(), out number))
+                Console.Write("Chybně zadané celé číslo!! \nZadejte znovu: ");
+
+            Console.WriteLine("\nVýpočet... ");
+            uint i;
+            for (i = 0; i < binNumber.Length; i++)
+            {
+                binNumber[binNumber.Length - 1 - i] = number % 2;
+                Console.WriteLine("Celá část: {0} | Zbytek po dělení: {1}", number, binNumber[binNumber.Length - 1 - i]);
+                number = (number - binNumber[i]) / 2;
+                if (number == 0)
+                    break;
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Výsledek: ");
+            Console.Write("BIN: ");
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(binNumber[i - j]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
